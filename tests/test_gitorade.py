214e14aa55d6678210ba485014ceb12a1eb4bc43
Marouane Skandaji <marouane.skandaji@gmail.com>
2022-12-24 13:19:30 +0100

    [test ]: add unit tests

--- tests/test_gitorade.py
+++ tests/test_gitorade.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 
 import shutil
 import subprocess
+from unittest.mock import patch
 
 import pytest
 
-from gitorade import find_git
+from gitorade import _add_commit_option, _git_commit, find_git
 
 
 def test_find_git() -> None:
@@ -39,3 +40,40 @@ def test_find_git_version_not_supported() -> None:
     with pytest.raises(RuntimeError) as e:
         find_git()
     assert str(e.value) == f'git version {git_version} is not supported'
+
+
+@pytest.mark.parametrize(
+    'option, message, expected',
+    [
+        ('feat', 'test', '[feat]: test'),
+        ('fix', 'test', '[fix]: test'),
+        ('docs', 'test', '[docs]: test'),
+        ('style', 'test', '[style]: test'),
+        ('refactor', 'test', '[refactor]: test'),
+        ('perf', 'test', '[perf]: test'),
+        ('test', 'test', '[test]: test'),
+        ('chore', 'test', '[chore]: test'),
+        ('revert', 'test', '[revert]: test'),
+        ('build', 'test', '[build]: test'),
+        ('ci', 'test', '[ci]: test'),
+        ('release', 'test', '[release]: test'),
+        ('other', 'test', '[other]: test'),
+        ('other', None, None),
+        ('other', '', None),
+    ],
+)
+def test__add_commit_option(option: str, message: str | None, expected: str | None) -> None:
+    """
+    Test _add_commit_option, which adds the commit option to the git commit message,
+    if the option is feat, fix, etc.
+    """
+    assert _add_commit_option(option, message) == expected
+
+
+@pytest.mark.skip(reason='not implemented')
+def test__git_commit() -> None:
+    """
+    Test git commit, mock the git commit command
+    """
+    subprocess.check_output = lambda x: b'test'
+    assert _git_commit('test') == 'test'
